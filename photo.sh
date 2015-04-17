83b6ba45fa811dde32e93ffb272a6f5175c31099
Simon Foster <funkysi1701@gmail.com>
2015-04-17 18:58:28 +0100

    copy to copy.com fixed

--- photo.sh
+++ photo.sh
@@ -4,4 +4,4 @@ DATE=$(date +"%Y-%m-%d_%H%M")
 
 raspistill -q 10 -vf -hf -a a$DATE -o /var/www/$DATE.jpg
 cd /home/pi/copy/armv6h
-./CopyCmd Cloud -username= -password='' put /var/www/$DATE.jpg /:
+./CopyCmd Cloud -username= -password='' put /var/www/$DATE.jpg /
